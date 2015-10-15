6afbfdbe254ba635e953362bcfab38091111c16c
dimgrek <dimgrek@gmail.com>
2015-10-15 11:30:47 +0300

    Everything works now

--- WeatherAndroidXamarin/MainActivity.cs
+++ WeatherAndroidXamarin/MainActivity.cs
@@ -11,15 +11,25 @@ namespace WeatherAndroidXamarin
     {
         private TextView _currentDateTextView;
         private EditText _editText;
+        private TextView _humidityTextView;
+        private TextView _humidityTheDayAfterAfterTomorrowTextView;
+        private TextView _humidityTheDayAfterTomorrowTextView;
+        private TextView _humidityTomorrowTextView;
         private TextView _temperatureTextView;
+        private TextView _temperatureTheDayAfterAfterTomorrowTextView;
+        private TextView _temperatureTheDayAfterTomorrowTextView;
+        private TextView _temperatureTomorrowTextView;
+        private TextView _theDayAfterAfterTomorrowDateTextView;
+        private TextView _theDayAfterTomorrowDateTextView;
+        private TextView _tomorrowDateTextView;
         private WeatherService _ws;
-        int count = 1;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             _ws = new WeatherService();
             var now = DateTime.Now;
+
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
@@ -28,9 +38,28 @@ namespace WeatherAndroidXamarin
             // and attach an event to it
             var button = FindViewById<Button>(Resource.Id.MyButton);
             _editText = FindViewById<EditText>(Resource.Id.EditText);
+
             _currentDateTextView = FindViewById<TextView>(Resource.Id.CurrentDateTextView);
+            _tomorrowDateTextView = FindViewById<TextView>(Resource.Id.TomorrowDateTextView);
+            _theDayAfterTomorrowDateTextView = FindViewById<TextView>(Resource.Id.DayAfterTomorrowDateTextView);
+            _theDayAfterAfterTomorrowDateTextView = FindViewById<TextView>(Resource.Id.DayAfterAfterTomorrowDateTextView);
+
             _temperatureTextView = FindViewById<TextView>(Resource.Id.TemperatureTextView);
+            _temperatureTomorrowTextView = FindViewById<TextView>(Resource.Id.TemperatureTomorrowTextView);
+            _temperatureTheDayAfterTomorrowTextView = FindViewById<TextView>(Resource.Id.TemperatureTheDayAfterTomorrowTextView);
+            _temperatureTheDayAfterAfterTomorrowTextView = FindViewById<TextView>(Resource.Id.TemperatureTheDayAfterAfterTomorrowTextView);
+
+            _humidityTextView = FindViewById<TextView>(Resource.Id.HumidityTextView);
+            _humidityTomorrowTextView = FindViewById<TextView>(Resource.Id.HumidityTomorrowTextView);
+            _humidityTheDayAfterTomorrowTextView = FindViewById<TextView>(Resource.Id.HumidityTheDayAfterTomorrowTextView);
+            _humidityTheDayAfterAfterTomorrowTextView = FindViewById<TextView>(Resource.Id.HumidityTheDayAfterAfterTomorrowTextView);
+
+
             _currentDateTextView.Text = DateTime.Today.ToString("M");
+            _tomorrowDateTextView.Text = now.AddDays(1).ToString("dd - MM - yy");
+            _theDayAfterTomorrowDateTextView.Text = now.AddDays(2).ToString("dd-MM-yy");
+            _theDayAfterAfterTomorrowDateTextView.Text = now.AddDays(3).ToString("dd-MM-yy");
+
 
             button.Click += SearchWeather;
         }
@@ -39,6 +68,20 @@ namespace WeatherAndroidXamarin
         {
             var result = await _ws.GetForecastForToday(_editText.Text);
             _temperatureTextView.Text = result.Temperature + "°C";
+            _humidityTextView.Text = result.Humidity + "%";
+
+            var tomorrow = await _ws.GetForecastForTomorrow(_editText.Text);
+            _temperatureTomorrowTextView.Text = tomorrow.Temperature + "°C";
+            _humidityTomorrowTextView.Text = tomorrow.Humidity + "%";
+
+            var theDayAfterTomorrow = await _ws.GetForecastForTheDayAfterTomorrow(_editText.Text);
+            _temperatureTheDayAfterTomorrowTextView.Text = theDayAfterTomorrow.Temperature + "°C";
+            _humidityTheDayAfterTomorrowTextView.Text = theDayAfterTomorrow.Humidity + "%";
+
+            var theDayAfterAfterTomorrow = await _ws.GetForecastForTheDayAfterAfterTomorrow(_editText.Text);
+            _temperatureTheDayAfterAfterTomorrowTextView.Text = theDayAfterAfterTomorrow.Temperature + "°C";
+            _humidityTheDayAfterTomorrowTextView.Text = theDayAfterAfterTomorrow.Humidity + "%";
+
         }
     }
 }
--- WeatherAndroidXamarin/Resources/Resource.Designer.cs
+++ WeatherAndroidXamarin/Resources/Resource.Designer.cs
@@ -63,9 +63,6 @@ namespace WeatherAndroidXamarin
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060005
-			public const int 3dfTextView = 2131099653;
-			
 			// aapt resource value: 0x7f060002
 			public const int CurrentDateTextView = 2131099650;
 			
@@ -81,14 +78,35 @@ namespace WeatherAndroidXamarin
 			// aapt resource value: 0x7f060003
 			public const int HumidityTextView = 2131099651;
 			
+			// aapt resource value: 0x7f06000b
+			public const int HumidityTheDayAfterAfterTomorrowTextView = 2131099659;
+			
+			// aapt resource value: 0x7f06000a
+			public const int HumidityTheDayAfterTomorrowTextView = 2131099658;
+			
+			// aapt resource value: 0x7f060009
+			public const int HumidityTomorrowTextView = 2131099657;
+			
 			// aapt resource value: 0x7f060001
 			public const int MyButton = 2131099649;
 			
 			// aapt resource value: 0x7f060004
 			public const int TemperatureTextView = 2131099652;
 			
+			// aapt resource value: 0x7f06000e
+			public const int TemperatureTheDayAfterAfterTomorrowTextView = 2131099662;
+			
+			// aapt resource value: 0x7f06000d
+			public const int TemperatureTheDayAfterTomorrowTextView = 2131099661;
+			
+			// aapt resource value: 0x7f06000c
+			public const int TemperatureTomorrowTextView = 2131099660;
+			
 			// aapt resource value: 0x7f060006
 			public const int TomorrowDateTextView = 2131099654;
+			
+			// aapt resource value: 0x7f060005
+			public const int dfTextView = 2131099653;
 			
 			static Id()
 			{
